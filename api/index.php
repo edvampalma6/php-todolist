8325067c2be372681736b00b5ef316f9554cd888
Edvam Palma da Silveira <ed.psilveira6@gmail.com>
2015-11-10 22:33:49 +0000

    First List

--- api/index.php
+++ api/index.php
@@ -1,11 +1,16 @@
 <?php
 require 'vendor/autoload.php';
 $app = new \Slim\Slim();
+
 $app->get('/', function() use ( $app ) {
-    echo "Welcome to REST API";
+    echo "Welcome to REST API";  //http://hostname/api
 });
+
 $app->get('/hello/:name', function($name) use ( $app ) {
-    echo "Hi $name, welcome to the REST API's";
+    echo "Hi $name, welcome to the REST API's";  //rota hello
 });
+
+
+
 $app->run();
 ?>
