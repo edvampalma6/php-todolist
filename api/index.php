7bfcd1e1e75117081755aa5b35cf42cdf452bf04
Edvam Palma da Silveira <ed.psilveira6@gmail.com>
2015-11-11 00:04:30 +0000

    Second List

--- api/index.php
+++ api/index.php
@@ -1,16 +1,21 @@
 <?php
 require 'vendor/autoload.php';
 $app = new \Slim\Slim();
-
+//http://hostname/api/
 $app->get('/', function() use ( $app ) {
-    echo "Welcome to REST API";  //http://hostname/api
+    echo "Welcome to Task REST API";
 });
-
-$app->get('/hello/:name', function($name) use ( $app ) {
-    echo "Hi $name, welcome to the REST API's";  //rota hello
+$app->get('/tasks', function() use ( $app ) {
+    $tasks = getTasks();
+    echo json_encode($tasks);
 });
-
-
-
+//TODO move it to a DAO class
+function getTasks() {
+    $tasks = array (
+        array('id'=>1,'description'=>'Learn REST','done'=>false),
+        array('id'=>2,'description'=>'Learn JavaScript','done'=>false)
+    );
+    return $tasks;
+}
 $app->run();
 ?>
